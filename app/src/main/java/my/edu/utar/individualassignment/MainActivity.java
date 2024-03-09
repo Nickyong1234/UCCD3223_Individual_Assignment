f017bbe75e8a413a511ba56a76bdad8f7f8becb9
Nickyong1234 <nickyong1234@gmail.com>
2024-03-09 20:45:13 +0800

    Basic UI for first page

--- app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
+++ app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text1 = new TextView(this);
+        /*TextView text1 = new TextView(this);
         TextView text2 = new TextView(this);
         text1.setText("TextView is used to deliver a text message");
         text2.setText("More than 1 TextView can be used in one layout");
@@ -21,6 +21,6 @@ public class MainActivity extends AppCompatActivity {
         ll.setOrientation(LinearLayout.VERTICAL);
         ll.addView(text1);
         ll.addView(text2);
-        setContentView(ll);
+        setContentView(ll);*/
     }
 }
