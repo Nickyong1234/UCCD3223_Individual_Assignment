3dca926c1adcc5692564c3723e1d9edecb74eea1
Nickyong1234 <nickyong1234@gmail.com>
2024-03-16 09:56:42 +0800

    changing buttons layout from Horizontal to Vertical

--- app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
+++ app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
@@ -2,7 +2,10 @@ package my.edu.utar.individualassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,14 +16,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*TextView text1 = new TextView(this);
-        TextView text2 = new TextView(this);
-        text1.setText("TextView is used to deliver a text message");
-        text2.setText("More than 1 TextView can be used in one layout");
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        ll.addView(text1);
-        ll.addView(text2);
-        setContentView(ll);*/
+
+        // Get the button
+        Button bcompare = (Button) findViewById(R.id.bcompare);
+        // Create a button listener
+        bcompare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an intent to start the second activity
+                Intent intent = new Intent(MainActivity.this, Compare.class);
+                // Start the activity
+                startActivity(intent);
+            }
+        });
+
+        // Get the button
+        Button border = (Button) findViewById(R.id.border);
+        // Create a button listener
+        border.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an intent to start the second activity
+                Intent intent = new Intent(MainActivity.this, Order.class);
+                // Start the activity
+                startActivity(intent);
+            }
+        });
+
+        // Get the button
+        Button bcompose = (Button) findViewById(R.id.bcompose);
+        // Create a button listener
+        bcompose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an intent to start the second activity
+                Intent intent = new Intent(MainActivity.this, Compose.class);
+                // Start the activity
+                startActivity(intent);
+            }
+        });
     }
 }
