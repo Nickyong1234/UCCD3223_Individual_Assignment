52144be1b84096a785a805827fecb27deef7b5a5
Nickyong1234 <nickyong1234@gmail.com>
2024-03-18 20:37:40 +0800

    Initial UI for compare numbers

--- app/src/main/java/my/edu/utar/individualassignment/Compare.java
+++ app/src/main/java/my/edu/utar/individualassignment/Compare.java
@@ -3,6 +3,7 @@ package my.edu.utar.individualassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Compare extends AppCompatActivity {
 
@@ -10,6 +11,27 @@ public class Compare extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare);
+
+        // declare a new int variable
+        int BigOrSmall;
+
+
+        // Random either Bigger or Smaller
+        int random = (int) (Math.random() * 2);
+        if (random == 0) {
+            // Bigger
+            BigOrSmall = 1;
+            //Replace CompareTitle text with "Bigger"
+            TextView CompareTitle = findViewById(R.id.CompareQuestion);
+            CompareTitle.setText(R.string.please_select_a_bigger_number);
+
+        } else {
+            // Smaller
+            BigOrSmall = 0;
+            //Replace CompareTitle text with "Smaller"
+            TextView CompareTitle = findViewById(R.id.CompareQuestion);
+            CompareTitle.setText(R.string.please_select_a_smaller_number);
+        }
 
     }
 }
--- app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
+++ app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
@@ -1,13 +1,11 @@
 package my.edu.utar.individualassignment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
