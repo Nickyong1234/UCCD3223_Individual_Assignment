61e7fbc274f8affa43e449b45568ef7851feaf35
Nickyong1234 <nickyong1234@gmail.com>
2024-03-19 03:41:19 +0800

    More changes to Compare (Specifically to randomize numbers after Button Listener)

--- app/src/main/java/my/edu/utar/individualassignment/Compare.java
+++ app/src/main/java/my/edu/utar/individualassignment/Compare.java
@@ -1,13 +1,13 @@
 package my.edu.utar.individualassignment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Compare extends AppCompatActivity {
 
@@ -17,13 +17,16 @@ public class Compare extends AppCompatActivity {
         setContentView(R.layout.activity_compare);
 
         // declare a new int variable
-        int BigOrSmall;
+        //int BigOrSmall;
+        AtomicInteger BigOrSmall = new AtomicInteger();
+        AtomicInteger randomleft = new AtomicInteger();
+        AtomicInteger randomright = new AtomicInteger();
 
         // Random either Bigger or Smaller
         int random = (int) (Math.random() * 2);
         if (random == 0) {
             // Bigger
-            BigOrSmall = 1;
+            BigOrSmall.set(1);
             //Replace CompareTitle text with "Bigger"
             TextView CompareTitle = findViewById(R.id.ComposeQuestion);
             CompareTitle.setText(R.string.please_select_a_bigger_number);
@@ -31,7 +34,7 @@ public class Compare extends AppCompatActivity {
         }
         else {
             // Smaller
-            BigOrSmall = 0;
+            BigOrSmall.set(0);
             //Replace CompareTitle text with "Smaller"
             TextView CompareTitle = findViewById(R.id.ComposeQuestion);
             CompareTitle.setText(R.string.please_select_a_smaller_number);
@@ -42,84 +45,103 @@ public class Compare extends AppCompatActivity {
         Button button_right = findViewById(R.id.button_right);
 
         // Create 2 random integers and set text to the buttons
-        int randomleft = (int) (Math.random() * 100);
-        int randomright = (int) (Math.random() * 100);
+        randomleft.set((int) (Math.random() * 100));
+        randomright.set((int) (Math.random() * 100));
         button_left.setText(String.valueOf(randomleft));
         button_right.setText(String.valueOf(randomright));
 
         // Create a button listener
-        button_left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an intent to start the compare activity
-                if (BigOrSmall == 1 /*Bigger*/) {
-                    if (randomleft > randomright){
-                        // To be replaced with another activity (Correct View)
-                        Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        // To be replaced with another activity (Incorrect View)
-                        Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
-                    }
+        button_left.setOnClickListener(view -> {
+            // Create an intent to start the compare activity
+            if (BigOrSmall.get() == 1 /*Bigger*/) {
+                if (randomleft.get() > randomright.get()){
+                    // To be replaced with another activity (Correct View)
+                    Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
                 }
-                else /*Smaller*/{
-                    if (randomleft < randomright){
-                        // To be replaced with another activity (Correct View)
-                        Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        // To be replaced with another activity (Incorrect View)
-                        Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
-                    }
+                else {
+                    // To be replaced with another activity (Incorrect View)
+                    Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
                 }
-                Intent intent = new Intent(Compare.this, Compare.class);
-                // Start the activity
-                startActivity(intent);
             }
+            else /*Smaller*/{
+                if (randomleft.get() < randomright.get()){
+                    // To be replaced with another activity (Correct View)
+                    Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    // To be replaced with another activity (Incorrect View)
+                    Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            // Randomize BigOrSmall, randomleft, and randomright
+            BigOrSmall.set((int) (Math.random() * 2));
+            randomleft.set((int) (Math.random() * 100));
+            randomright.set((int) (Math.random() * 100));
+            // Update the text of the buttons and ComposeQuestion
+            button_left.setText(String.valueOf(randomleft));
+            button_right.setText(String.valueOf(randomright));
+            if (BigOrSmall.get() == 1) {
+                TextView CompareTitle = findViewById(R.id.ComposeQuestion);
+                CompareTitle.setText(R.string.please_select_a_bigger_number);
+            }
+            else {
+                TextView CompareTitle = findViewById(R.id.ComposeQuestion);
+                CompareTitle.setText(R.string.please_select_a_smaller_number);
+            }
+            /*Intent intent = new Intent(Compare.this, Compare.class);
+            // Start the activity
+            startActivity(intent);*/
         });
 
         // Create a button listener
-        button_right.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an intent to start the compare activity
-                if (BigOrSmall == 1 /*Bigger*/) {
-                    if (randomright > randomleft){
-                        // To be replaced with another activity (Correct View)
-                        Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        // To be replaced with another activity (Incorrect View)
-                        Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
-                    }
+        button_right.setOnClickListener(view -> {
+            // Create an intent to start the compare activity
+            if (BigOrSmall.get() == 1 /*Bigger*/) {
+                if (randomright.get() > randomleft.get()){
+                    // To be replaced with another activity (Correct View)
+                    Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
                 }
-                else /*Smaller*/{
-                    if (randomright < randomleft){
-                        // To be replaced with another activity (Correct View)
-                        Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        // To be replaced with another activity (Incorrect View)
-                        Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
-                    }
+                else {
+                    // To be replaced with another activity (Incorrect View)
+                    Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
                 }
-                Intent intent = new Intent(Compare.this, Compare.class);
-                // Start the activity
-                startActivity(intent);
             }
+            else /*Smaller*/{
+                if (randomright.get() < randomleft.get()){
+                    // To be replaced with another activity (Correct View)
+                    Toast.makeText(Compare.this, "Correct!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    // To be replaced with another activity (Incorrect View)
+                    Toast.makeText(Compare.this, "Wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            // Randomize BigOrSmall, randomleft, and randomright
+            BigOrSmall.set((int) (Math.random() * 2));
+            randomleft.set((int) (Math.random() * 100));
+            randomright.set((int) (Math.random() * 100));
+            // Update the text of the buttons and ComposeQuestion
+            button_left.setText(String.valueOf(randomleft));
+            button_right.setText(String.valueOf(randomright));
+            if (BigOrSmall.get() == 1) {
+                TextView CompareTitle = findViewById(R.id.ComposeQuestion);
+                CompareTitle.setText(R.string.please_select_a_bigger_number);
+            }
+            else {
+                TextView CompareTitle = findViewById(R.id.ComposeQuestion);
+                CompareTitle.setText(R.string.please_select_a_smaller_number);
+            }
+            /*Intent intent = new Intent(Compare.this, Compare.class);
+            // Start the activity
+            startActivity(intent);*/
         });
 
         // Get the BackButton button
         Button back = findViewById(R.id.BackButton);
         // Create a button listener
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an intent to start the main activity
-                Intent intent = new Intent(Compare.this, MainActivity.class);
-                // Start the activity
-                startActivity(intent);
-            }
+        back.setOnClickListener(view -> {
+            // End the activity
+            finish();
         });
     }
 
