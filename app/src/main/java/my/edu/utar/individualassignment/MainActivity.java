beaf264760b7924644b59bada8426d785feb9691
Nickyong1234 <nickyong1234@gmail.com>
2024-03-19 18:28:35 +0800

    Also check for zeros

--- app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
+++ app/src/main/java/my/edu/utar/individualassignment/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Get the button
         Button bcompare = (Button) findViewById(R.id.bcompare);
         // Create a button listener
